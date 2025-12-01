b2c13de155618f40ac6ca24e1702913f40388092
Sophie L <slandau@forestreet.com>
2025-12-01 08:36:50 +0000

    feat: day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ mod solutions;
 mod utils;
 
 fn main() {
-    println!("I'm still setting up this repo!!!");
-    println!("Day 1, part 1: {:?}", solutions::day_1::day_1());
+    solutions::day_1::day_1();
 }
--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -1,15 +1,39 @@
 use crate::utils::{get_input::get_aoc_input, parsing::split_lines};
 
 pub fn day_1() {
-    println!("This is a placeholder for AOC 2025 Day 1!");
-    let input = split_lines(get_aoc_input(2025, 1));
+    println!("Part 1: {:?}", part_1());
 }
+
+fn part_1() -> i16 {
+    let input = split_lines(get_aoc_input(2025, 1));
+    let mut current_pos: i16 = 50;
+    let mut at_zero: i16 = 0;
+    for instruction in input {
+        let direction = &instruction[..1];
+        let how_far: i16 =
+            str::parse::<i16>(&instruction[1..]).expect("Unable to convert how_far to number");
+        if direction == "L" {
+            current_pos -= how_far;
+        } else if direction == "R" {
+            current_pos += how_far
+        } else {
+            panic!("Invalid direction!")
+        }
+        if current_pos < 0 || current_pos > 99 {
+            current_pos = ((current_pos % 100) + 100) % 100;
+        }
+        if current_pos == 0 {
+            at_zero += 1
+        }
+    }
+    return at_zero;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_part_1() {
-        let test = get_aoc_input(2025, 1);
-        println!("{:?}", test);
+        assert_eq!(part_1(), 3);
     }
 }
